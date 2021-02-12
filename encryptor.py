e6188f9c512e1621e5b47dad4be326b51cbdc7e9
Zac Nolan <zac1i1nolan@gmail.com>
2021-02-12 17:46:06 +0000

    Add files via upload

--- encryptor.py
+++ encryptor.py
@@ -1,1 +1,25 @@
-pass
+decrypted = b"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ,./1234567890 "
+encrypted = b"0896745231`-+=/.,<>?'#~@}{[]zcxbvmnadsgfhjlkpioyurteAZCXBVMNLJIOTY"
+
+encrypt_table = bytes.maketrans(decrypted, encrypted)
+decrypt_table = bytes.maketrans(encrypted, decrypted)
+
+result = ''
+choice = ''
+message = ''
+
+while choice != '0':
+    choice = input("\n Do you want to encrypt or decrypt the message?\n 1 to encrypt, 2 to decrypt or 0 to exit program. ")
+
+    if choice == '1':
+        message = input('\nEnter message for encryption: ')
+        result = message.translate(encrypt_table)
+        print(result + '\n\n')
+
+    elif choice == '2':
+        message = input('\nEnter message to decrypt: ')
+        result = message.translate(decrypt_table)
+        print(result + '\n\n')
+
+    elif choice != '0':
+        print('You have entered an invalid input, please try again. \n\n')
